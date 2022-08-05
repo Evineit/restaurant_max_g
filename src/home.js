b89b4456d3cd1fda24b11e76a150feea7a789490
Kevin G. Rodriguez Parra <seidaikevin@gmail.com>
2022-08-05 12:25:27 -0500

    Refactor and style home tab

--- src/home.js
+++ src/home.js
@@ -1,15 +1,30 @@
-import './style.css'
+import "./style.css";
 
-export function generateHome() {
-    const heading = document.createElement("h1")
-    const para = document.createElement("p1")
+export default function home() {
+  const hero = document.createElement("div");
+  hero.classList.add("hero")
+  const para = document.createElement("p");
 
-    heading.textContent = "Hi, I'm Max G."
-    para.innerHTML = `I take brief but concise notes from the best cooking books of all time.
+  para.innerHTML = `I take brief but concise notes from the best cooking books of all time.
     Them, in my monthly newsletter, I share the big ideas you can use to
-    cook better and be smarter.
+    <em>cook better</em> and <em>be creative</em>.
     <br>
-    I'm learning a lot and so will you.`
+    <br>
+    I'm learning a lot and so will you.`;
+  hero.append(heroTitle(), para, btnLearnMore());
+  return hero;
+}
 
-    return [nav, heading, para]
+function heroTitle() {
+  const heading = document.createElement("h1");
+  heading.textContent = "Hi, I'm Max G.";
+  heading.classList.add("hero-title");
+  return heading;
+}
+
+function btnLearnMore() {
+    const btn = document.createElement("button")
+    btn.classList.add("btn-cta")
+    btn.textContent = "Learn more"
+    return btn
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import navbar from './nav.js'
+import home from './home.js'
 
 document.addEventListener('DOMContentLoaded', function(){
     const content = document.getElementById("content")
-    content.append(navbar())
+    content.append(navbar(), home())
 })
 
 
