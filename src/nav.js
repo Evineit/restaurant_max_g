ccf0b6f19d8608c51291f163b8b0930c94e68341
Kevin G. Rodriguez Parra <seidaikevin@gmail.com>
2022-08-05 11:35:49 -0500

    Change navbar style

--- src/nav.js
+++ src/nav.js
@@ -4,21 +4,24 @@ import Logo from "./logo.png";
 export default function navbar() {
   const nav = document.createElement("nav");
   const ul = document.createElement("ul");
-  const home = createListItem("home");
+  const home = createListItem("home", true);
   const menu = createListItem("menu");
   const contact = createListItem("contact");
   const img = document.createElement("img");
 
   img.src = Logo;
   img.alt = "logo";
-  
+
   ul.append(home, menu, contact);
   nav.append(img, ul);
   return nav;
 }
 
-function createListItem(text) {
+function createListItem(text, isSelected = false) {
   const li = document.createElement("li");
+  if (isSelected) {
+    li.classList.add("selected");
+  }
   li.textContent = text;
   return li;
 }
